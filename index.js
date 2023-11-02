0a29cf3d5f445cf68aa13be5975dee3c7ba040ab
anubhav <anubhavadhikari15@gmail.com>
2023-11-02 18:52:57 +0530

    c1 - added all req-header-parser microservice project files

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/api/whoami', function (req, res) {
+  res.json({ ipaddress:  req.ip, language: req.headers['accept-language'], software: req.headers['user-agent']});
+});
+
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
