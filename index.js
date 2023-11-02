e4a90792e15c0956466cddbb9605ac048eb2a356
anubhav <anubhavadhikari15@gmail.com>
2023-11-02 20:44:12 +0530

    c2 - updated ip retrieval method and other minor changes

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ app.use(cors({ optionsSuccessStatus: 200 }));
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/api/whoami', function (req, res) {
-  res.json({ ipaddress:  req.ip, language: req.headers['accept-language'], software: req.headers['user-agent']});
+app.get('/api/whoami', (req, res) => {
+  res.json({ ipaddress: req.ip, language: req.headers['accept-language'], software: req.headers['user-agent']});
 });
 
-const listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
